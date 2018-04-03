e53025a1940b38db88b948408a13acda116733d2
fahmi <fahmi.azizh@gmail.com>
2018-04-03 16:18:01 +0700

    revisi controller api

--- application/modules/masterdata/controllers/lapangan.php
+++ application/modules/masterdata/controllers/lapangan.php
@@ -12,20 +12,21 @@ class Lapangan extends MY_Controller {
 	public function index()
 	{
 		$param = array('company_code' => 'RDCNT');
-		$data = json_decode(($this->curl->simple_get($this->API.'Kontak/data_field', $param)), true);
+		$data = json_decode(($this->curl->simple_get($this->API.'Master_data/data_field', $param)), true);
 		$this->templates->assign( 'data_lapangan', $data);
     	$this->layout('lapangan/lists', '');
 	}
 	public function add()
 	{
 		$param_fieldtype = array('code_category' => 'FLT');
-		$field_type = json_decode(($this->curl->simple_get($this->API.'Kontak/code_bycategory', $param_fieldtype)), true);
+		$field_type = json_decode(($this->curl->simple_get($this->API.'Master_data/code_bycategory', $param_fieldtype)), true);
+		print_r($field_type);
 
 		$param_fieldroom = array('code_category' => 'FLR');
-		$field_room = json_decode(($this->curl->simple_get($this->API.'Kontak/code_bycategory', $param_fieldroom)), true);
+		$field_room = json_decode(($this->curl->simple_get($this->API.'Master_data/code_bycategory', $param_fieldroom)), true);
 
 		$param_day = array('code_category' => 'DYT');
-		$day_type = json_decode(($this->curl->simple_get($this->API.'Kontak/code_bycategory', $param_day)), true);
+		$day_type = json_decode(($this->curl->simple_get($this->API.'Master_data/code_bycategory', $param_day)), true);
 
 		$this->templates->assign( 'day_type', $day_type);
 		$this->templates->assign( 'field_room', $field_room);
